813d6d051beb96d41e4001dc406822b6823130a8
Rejeesh.T.S <rejeesh@gmail.com>
2018-07-10 19:38:36 +0530

    First commit

---
+++ g_search/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace g_search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtCriteria = new System.Windows.Forms.TextBox();
+            this.txtFileType = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRefFolder = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDestFolder = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(-13, -2);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(883, 328);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(711, 355);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCriteria
+            // 
+            this.txtCriteria.Location = new System.Drawing.Point(110, 355);
+            this.txtCriteria.Name = "txtCriteria";
+            this.txtCriteria.Size = new System.Drawing.Size(542, 20);
+            this.txtCriteria.TabIndex = 2;
+            // 
+            // txtFileType
+            // 
+            this.txtFileType.Location = new System.Drawing.Point(110, 389);
+            this.txtFileType.Name = "txtFileType";
+            this.txtFileType.Size = new System.Drawing.Size(43, 20);
+            this.txtFileType.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 352);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Criteria:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 389);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File type (without \'.\'):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 422);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Reference folder:";
+            // 
+            // txtRefFolder
+            // 
+            this.txtRefFolder.Location = new System.Drawing.Point(110, 419);
+            this.txtRefFolder.Name = "txtRefFolder";
+            this.txtRefFolder.Size = new System.Drawing.Size(273, 20);
+            this.txtRefFolder.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 453);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Destination folder:";
+            // 
+            // txtDestFolder
+            // 
+            this.txtDestFolder.Location = new System.Drawing.Point(110, 450);
+            this.txtDestFolder.Name = "txtDestFolder";
+            this.txtDestFolder.Size = new System.Drawing.Size(273, 20);
+            this.txtDestFolder.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 520);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDestFolder);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtRefFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFileType);
+            this.Controls.Add(this.txtCriteria);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtCriteria;
+        private System.Windows.Forms.TextBox txtFileType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRefFolder;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDestFolder;
+    }
+}
+
---
+++ g_search/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Media;
+
+namespace g_search
+{
+    public partial class Form1 : Form
+    {
+        string nextURL = null;
+        bool isFirstPage = true;
+        int numPagesToProcess = 0;
+        string logFile = @"F:\rejeesh\V4U\log.txt";
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtRefFolder.Text = @"F:\rejeesh\V4U\Files";
+            txtDestFolder.Text = @"F:\rejeesh\V4U\TodaysFiles";
+
+            Trace.AutoFlush = true;
+            Trace.Listeners.Add(new TextWriterTraceListener(logFile));
+
+            this.webBrowser1.Navigate("https://www.google.com");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = this.txtCriteria.Text;
+            string queryEncoded = HttpUtility.UrlEncode(query);
+            nextURL = "https://www.google.com/search?q=" + queryEncoded;
+            this.webBrowser1.Navigate(nextURL);
+            isFirstPage = true;
+        }
+
+        static IEnumerable<HtmlElement> ElementsByClass(HtmlDocument doc, string className)
+        {
+            foreach (HtmlElement e in doc.All)
+                if (e.GetAttribute("className") == className)
+                    yield return e;
+        }
+
+
+
+
+        bool downloadFile(string url)
+        {
+            string fileName = Path.GetFileName(url);
+            fileName = HttpUtility.UrlDecode(fileName);
+
+            string expectedExt = "." + this.txtFileType.Text;
+
+            try
+            {
+
+                if (Path.GetExtension(fileName).ToLower() == expectedExt.ToLower())
+                {
+                    downloadDirect(url);
+                }
+                else
+                {
+                    downloadFromStream(url);
+                }
+            }
+            catch(Exception e)
+            {
+                Trace.TraceInformation("Error " + fileName);
+            }
+
+            return true;
+        }
+
+        private bool downloadFromStream(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            String fileName = "";
+
+            try
+            {
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    var fn = response.Headers["Content-Disposition"].Split(new string[] { "=" }, StringSplitOptions.None)[1];
+                    string basePath = this.txtDestFolder.Text;
+                    var responseStream = response.GetResponseStream();
+
+                    fileName = fn.Replace('"', ' ');
+                    fileName = fileName.Trim();
+
+                    string expectedExt = "." + this.txtFileType.Text;
+                    if (Path.GetExtension(fileName).ToLower() != expectedExt.ToLower())
+                    {
+                        return true;
+                    }
+
+                    //If the file is already downloaded in destination folder, then skip it
+                    string path = Path.Combine(basePath, fileName);
+                    if (File.Exists(path)) return true;
+
+
+                    //If the file is already downloaded in reference folder, then skip it
+                    string refPath = Path.Combine(this.txtRefFolder.Text, fileName);
+                    if (File.Exists(refPath)) return true;
+
+
+
+                    using (var fileStream = File.Create(Path.Combine(basePath, fileName)))
+                    {
+                        responseStream.CopyTo(fileStream);
+                    }
+
+                    Trace.TraceInformation("Copied " + fileName);
+                }
+            }
+            catch (Exception e)
+            {
+                Trace.TraceInformation("Error: " + fileName);
+            }
+
+            return true;
+        }
+
+        private bool downloadDirect(string url)
+        {
+
+            string fileName = Path.GetFileName(url);
+            fileName = HttpUtility.UrlDecode(fileName);
+
+            //If the file already exists in the destination folder, then skip it
+            string destPath = Path.Combine(this.txtDestFolder.Text, fileName);
+
+            if (File.Exists(destPath)) return true;
+
+
+            //IF file is already downloaded in reference folder, then skip it
+            string refPath = Path.Combine(this.txtRefFolder.Text, fileName);
+
+            if (File.Exists(refPath)) return true;
+
+            try
+            {
+                WebClient Client = new WebClient();
+                Client.DownloadFile(url, destPath);
+            }
+            catch (Exception e)
+            {
+                Trace.TraceInformation("Error " + fileName);
+                return true;
+            }
+
+            Trace.TraceInformation("Copied " + fileName);
+
+            return true;
+
+        }
+
+
+        string getNextPage()
+        {
+
+            HtmlElement elHref = webBrowser1.Document.GetElementById("pnnext");
+
+            if (elHref == null) return "";
+
+            return elHref.GetAttribute("href");
+
+        }
+
+        int getNumberOfResultPages()
+        {
+            int val = 0;
+            HtmlElement elResult = webBrowser1.Document.GetElementById("resultStats");
+
+            if (elResult == null) return 0;
+
+            string[] res = elResult.InnerText.Split(' ');
+
+
+            int n = Array.IndexOf(res, "results");
+
+            if (n > 0)
+            {
+                string s = res[n - 1];
+                s = s.Replace(",", "");
+                val = Int32.Parse(s);
+            }
+
+            return val;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //if (String.IsNullOrEmpty(getNextPage()))
+            //    return;
+
+
+            if (isFirstPage == true)
+            {
+                isFirstPage = false;
+
+                if (getNumberOfResultPages() == 0)
+                    return;
+
+                if (getNumberOfResultPages() >= 100)
+                {
+                    numPagesToProcess = 10;
+                }
+
+                if (getNumberOfResultPages() < 100)
+                {
+                    numPagesToProcess = 5;
+                }
+
+                Trace.TraceInformation(txtCriteria.Text);
+            }
+
+
+            if (numPagesToProcess <= 0)
+            {
+                MessageBox.Show("Finished");
+                SystemSounds.Beep.Play();
+                return;
+            }
+            nextURL = getNextPage();
+
+            //Get all links to download
+
+            //HtmlElementCollection elc = webBrowser1.Document.GetElementsByTagName("span");
+            HtmlElementCollection elc = webBrowser1.Document.GetElementsByTagName("a");
+
+            foreach (HtmlElement elem in elc)
+            {
+                //string fileType = txtFileType.Text;
+                //if (txtFileType.Text.ToLower() == "docx")
+                //{
+                //    fileType = "doc";
+                //}
+
+                //if (txtFileType.Text.ToLower() == "pptx")
+                //{
+                //    fileType = "ppt";
+                //}
+                //string pattern = "[" + fileType.ToUpper() + "]";
+
+                //if (pattern == elem.InnerText)
+                {
+                    //HtmlElement h = elem.NextSibling;
+                    string link = elem.GetAttribute("href");
+
+                    if (String.IsNullOrEmpty(link)) continue;
+
+                    Uri u = new Uri(link);
+                    String s = u.Host;
+                    if (s.Contains("google") || s.Contains("youtube")) continue;
+
+                    if (String.IsNullOrEmpty(link)) continue;
+
+                    downloadFile(link);
+
+                }
+
+            }
+
+            numPagesToProcess--;
+            if(String.IsNullOrEmpty(nextURL))
+            {
+                MessageBox.Show("Finished. No next page.");
+                return;
+            }
+                this.webBrowser1.Navigate(nextURL);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+    }
+}
